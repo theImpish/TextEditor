58d1dd16b9b535fa13ffb8d41564b31a0ff6dc1d
theImpish <jorge.f.martins@outlook.com>
2024-07-23 22:05:36 +0100

    fixed character spacing and added line break

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main()
 		auto& grapheme = text.addCharacter(c);
 		std::cout << grapheme->grapheme;
 		grapheme->cFont = std::make_shared<CFont>(m_fonts.getFont("JetBrains"));
-		grapheme->cSize = std::make_shared<CSize>(20);
+		grapheme->cSize = std::make_shared<CSize>(40);
 		grapheme->cColour = std::make_shared<CColour>(47, 6, 1, 255);
 	}
 
@@ -37,8 +37,13 @@ int main()
 	while (render.running())
 	{
 		//render.window().draw(tTry);
-
+		int previousCharWidth = 0;
+		int previousX = 20;
+		int x = 0;
+		int y = 20;
 		int i = 0;
+		int charSpacing = 5;
+		int lineNumber = 0;
 		sf::Text renderChar;
 		for (auto& c : text.m_text)
 		{
@@ -46,29 +51,47 @@ int main()
 			renderChar.setString(c->grapheme);
 			renderChar.setFont(c->cFont->font);
 			renderChar.setCharacterSize(c->cSize->size);
-			int x = i * c->cSize->size + i * -5;
-			int y = 20;
+
+			if (previousX + previousCharWidth + charSpacing > render.window().getSize().x - 20)
+			{
+				lineNumber++;
+				previousX = 0;
+				y += 80;
+			}
+
+			x = previousX + previousCharWidth + charSpacing;
+			//y = 20;
+			previousX = x;
+			previousCharWidth = renderChar.getLocalBounds().width;
 			//if (x> render.window().getSize().x - c->cSize->size)
 			renderChar.setPosition(sf::Vector2f(x, y));
 			renderChar.setFillColor(c->cColour->colour);
 			render.window().draw(renderChar);
 
-			sf::Text coord;
-			coord.setFont(c->cFont->font);
-			coord.setString(std::to_string(x));
-			coord.setPosition(sf::Vector2f(x, 50));
-			coord.setCharacterSize(8);
-			coord.setFillColor(c->cColour->colour);
-			render.window().draw(coord);
+			//sf::Text coord;
+			//coord.setFont(c->cFont->font);
+			//coord.setString(std::to_string(x));
+			//coord.setPosition(sf::Vector2f(x, 90));
+			//coord.setCharacterSize(8);
+			//coord.setFillColor(c->cColour->colour);
+			//render.window().draw(coord);
 
-			sf::RectangleShape rect;
-			rect.setSize(sf::Vector2f(renderChar.getLocalBounds().width,renderChar.getLocalBounds().height));
-			rect.setOrigin(renderChar.getOrigin());
-			rect.setPosition(renderChar.getGlobalBounds().getPosition());
-			rect.setOutlineColor(sf::Color::White);
-			rect.setFillColor(sf::Color::Transparent);
-			rect.setOutlineThickness(1);
-			render.window().draw(rect);
+			//sf::Text charsize;
+			//charsize.setFont(c->cFont->font);
+			//charsize.setString(std::to_string(int(renderChar.getLocalBounds().width)));
+			//charsize.setPosition(sf::Vector2f(x, 120));
+			//charsize.setCharacterSize(8);
+			//charsize.setFillColor(c->cColour->colour);
+			//render.window().draw(charsize);
+
+			//sf::RectangleShape rect;
+			//rect.setSize(sf::Vector2f(renderChar.getLocalBounds().width,renderChar.getLocalBounds().height));
+			//rect.setOrigin(renderChar.getOrigin());
+			//rect.setPosition(renderChar.getGlobalBounds().getPosition());
+			//rect.setOutlineColor(sf::Color::White);
+			//rect.setFillColor(sf::Color::Transparent);
+			//rect.setOutlineThickness(1);
+			//render.window().draw(rect);
 
 		}
 
